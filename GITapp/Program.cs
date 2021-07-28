c381e319f2e3da4e5ae7a419637015e65b5f3a0f
arnab212 <70711617+arnab212@users.noreply.github.com>
2021-07-28 09:38:00 +0530

    changed

--- GITapp/Program.cs
+++ GITapp/Program.cs
@@ -6,7 +6,7 @@ namespace GITapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello and Welcome");
         }
     }
 }
